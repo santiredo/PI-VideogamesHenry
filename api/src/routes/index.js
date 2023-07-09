e6a135653631007e6062de7b7b7b5758a547ee41
santiRedondo <santi16redo@gmail.com>
2023-07-09 12:33:13 -0300

    CREANDO ENRUTAMIENTO

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 // Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+const getVideogame = require('../controllers/getVideogame');
+const getVideogameByName = require('../controllers/getVideogameByName');
+const getVideogameById = require('../controllers/getVideogameById');
+const postVideogame = require('../controllers/postVideogame');
+const getGenre = require('../controllers/getGenre');
 
 
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+// Ejemplo: router.use('/auth', authRouter);4
+
+router.get('/videogames', getVideogame);
+router.get('/videogame', getVideogameByName);
+router.get('/videogames/:id', getVideogameById);
+router.post('/videogames', postVideogame);
+router.get('/genres', getGenre);
 
 
 module.exports = router;
