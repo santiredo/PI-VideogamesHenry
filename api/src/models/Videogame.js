5108ecd046717351b7162c00182c7aafeceb9d91
santiRedondo <santi16redo@gmail.com>
2023-07-09 11:36:38 -0300

    CREANDO LOS MODELOS DE LA BASE DE DATOS

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,10 +3,42 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('Videogame', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
-  });
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    img: {
+      type: DataTypes.STRING,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isDateFormat(value) {
+          if (!/\d{4}-\d{2}-\d{2}/.test(value)) {
+            throw new Error('The field release must have the format YYYY-MM-DD.');
+          }
+        }
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  },{timestamps: false})
 };
