a9518d2c14c95ead9979f4031c54c201739a485d
santiRedondo <santi16redo@gmail.com>
2023-07-11 18:00:26 -0300

    CREANDO UN SEARCHBAR

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -7,7 +7,7 @@ export default function SearchBar (){
 
     return (
         <div className={style.searchBar}>
-            <input type="text" className={style.input}/>
+            <input type="text" className={style.input} />
             <button className={style.button}>Search</button>
         </div>
     )
