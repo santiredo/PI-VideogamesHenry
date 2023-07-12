cb5d78853a5fcb83b6965fdb3969026f7f24a939
santiRedondo <santi16redo@gmail.com>
2023-07-11 22:30:58 -0300

    COMENZANDO CON LOS FILTROS

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,12 +1,173 @@
 
-
+import { useState } from 'react'
+import './filters.css'
 
 
 export default function Filters(){
 
-    return(
-        <div>
+
+
+    const [order, setOrder] = useState(false)
+    const [gender, setGender] = useState(false)
+
+    const handleSelectOrder = (event) => {
+        const orderOptions = document.querySelector("#orderOptions")
+
+        if(!order) {
+            setOrder(true)
+            orderOptions.classList.remove('hiddenOptions');            
+            orderOptions.classList.add('orderActive');            
             
+        } else{
+            setOrder(false)
+
+            setTimeout(() => {
+                orderOptions.classList.remove('hideOrder');
+                orderOptions.classList.add('hiddenOptions')
+                
+            }, 500);
+            
+            orderOptions.classList.remove('orderActive')
+            orderOptions.classList.add('hideOrder')
+        }
+        event.currentTarget.classList.toggle('selectIdActive')
+    }
+
+    const handleSelectGender = (event) => {
+        const genderOptions = document.querySelector('#genderOptions');
+
+        if(!gender) {
+            setGender(true)
+            genderOptions.classList.remove('hiddenOptions');            
+            genderOptions.classList.add('genderActive');            
+            
+        } else{
+            setGender(false)
+
+            setTimeout(() => {
+                genderOptions.classList.remove('hideGender');
+                genderOptions.classList.add('hiddenOptions')
+                
+            }, 500);
+            
+            genderOptions.classList.remove('genderActive')
+            genderOptions.classList.add('hideGender')
+        }
+        event.currentTarget.classList.toggle('selectGenderActive');
+    }
+
+    return(
+        <div className='favFilters'>
+            <div className='selectBox'>
+                <div onClick={handleSelectOrder} className='selectId' id="orderSelect">
+                    <div className='selectedContent'>
+                        <p className='title'>Order</p>
+                    </div>
+                </div>
+                <div className='hiddenOptions' id="orderOptions">
+                    <div className="option">
+                        <p value="A">A-Z</p>
+                    </div>
+                    <div className="option">
+                        <p value="D">Z-A</p>
+                    </div>
+                    <div className="option">
+                        <p value="D">+Rating</p>
+                    </div>
+                    <div className="option">
+                            <p value="D">-Rating</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='selectBox'>
+                <div onClick={handleSelectGender} className='selectGender' id="genderSelect">
+                    <div className='selectedContent'>
+                        <p className='title'>Genres</p>
+                    </div>
+                </div>
+                <div className='hiddenOptions' id="genderOptions">
+                    <div className="option">
+                            <p value="ShowAll">Show all</p>
+                    </div>
+                    <div className="option">
+                            <p value="Male">Action</p>
+                    </div>
+                    <div className="option">
+                            <p value="Female">Adventure</p>
+                    </div>
+                    <div className="option">
+                            <p value="Genderless">RPG</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Indie</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Strategy</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Shooter</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Casual</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Simulation</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Puzzle</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Arcade</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Platformer</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Multiplayer</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Racing</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Sports</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Fighting</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Family</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Board games</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Educational</p>
+                    </div>
+                    <div className="option">
+                            <p value="unknown">Card</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='selectBox'>
+                <div onClick={handleSelectGender} className='selectGender' id="genderSelect">
+                    <div className='selectedContent'>
+                        <p className='title'>Origin</p>
+                    </div>
+                </div>
+                <div className='hiddenOptions' id="genderOptions">
+                    <div className="option">
+                            <p value="ShowAll">SHOW ALL</p>
+                    </div>
+                    <div className="option">
+                            <p value="Male">API</p>
+                    </div>
+                    <div className="option">
+                            <p value="Female">DATA BASE</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
