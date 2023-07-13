5ff68e7e1976326e8bec5645b020ec270dcd0ff1
santiRedondo <santi16redo@gmail.com>
2023-07-13 01:12:30 -0300

    CREANDO EL CSS PARA EL RENDERIZADO DE LAS CARDS

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,22 @@
 
 
+import style from './card.module.css'
 
 
-
-export default function Card(){
+export default function Card({id, name, image, genres}){
 
     return(
-        <div>
-            
+        <div className={style.cardDiv}>
+            <div className={style.imageDiv}>
+                <img src={image} alt="" />
+            </div>
+            <div className={style.data}>
+                <h2>{name}</h2>
+                {genres?.map((genre) => {
+                    return <h4 key={`${id}-${genre}`}>{genre}</h4>;
+                })}
+                <span>More details</span>
+            </div>
         </div>
     )
 }
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,12 +1,44 @@
 
-
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Card from '../Card/Card'
+import style from './cards.module.css'
 
 
 export default function Cards() {
 
+    const [videogames, setVideogames] = useState([])
+
+    const displayVideogames = async () => {
+
+        try {
+            const response = await axios('http://localhost:3001/videogames')
+            setVideogames(response.data)
+            
+        } catch (error) {
+            alert(error.message)            
+        }
+    }
+
+    useEffect(() => {
+        displayVideogames()
+        // eslint-disable-next-line
+    }, [])
+
     return (
-        <div>
+        <div className={style.cardsSection}>
+            {
+                videogames.map(game => {
+                    return(
+                        <Card
+                            key={game.id}
+                            name={game.name}
+                            image={game.image}
+                            genres={game.genres}
+                        />                        
+                    )
+                })
+            }
             
         </div>
     )
