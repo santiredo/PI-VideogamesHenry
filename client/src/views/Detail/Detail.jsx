5b76e76d5b9107c4c96d72fdbb7622e9805267a5
santiRedondo <santi16redo@gmail.com>
2023-07-16 15:38:06 -0300

    TERMINANDO CSS DE DETAIL

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -16,7 +16,13 @@ export default function Detail() {
         axios(`http://localhost:3001/videogames/${id}`)
         .then(({data}) => {
             if(data.name){
-                console.log(data)
+                console.log(data);
+                data.platforms = data.platforms.map(platform => {
+                    return `<p>${platform}</p>`
+                }).join('\n')
+                data.genres = data.genres.map(genre => {
+                    return `<p>${genre}</p>`
+                }).join('\n')
                 setVideogame(data);
                 setLoading(false)
             } else{
@@ -24,14 +30,19 @@ export default function Detail() {
             }
         });
 
+        let video = document.getElementById('backgroundVideo')
+        video.playbackRate = 0.5
+        console.log(video)
+
         return () =>{
             setVideogame({});
         };
     },[id])
 
+
     return (
         <div className={style.detailPage}>
-            <video autoPlay loop className={style.background} src={background}></video>
+            <video id="backgroundVideo" autoPlay loop className={style.background} src={background}></video>
 
                 {loading
                 ? (
@@ -42,10 +53,30 @@ export default function Detail() {
                 ) : (
                     <div className={style.videogameDiv}>
                         <h1>{videogame.name}</h1>
-                        <div className={style.detailsDiv}>
-                            <img src={videogame.image} alt="" />
-
+                        <div className={style.detailsContainer}>
+                            <div className={style.detailImage}>
+                                <img src={videogame.image} alt="" />
+                            </div>
+                            <div className={style.detailsDiv}>
+                                <div className={style.detailsInfo}>
+                                    <div>
+                                        <h5>Id: {videogame.id}</h5>
+                                        <h5>Released: {videogame.released}</h5>
+                                        <h5>Rating: {videogame.rating}</h5>
+                                        <h4>Genres: </h4>
+                                        <div className={style.platforms} dangerouslySetInnerHTML={{ __html: videogame.genres}}></div>
+                                    </div>
+                                    <div>
+                                        <h4>Platforms: </h4>
+                                        <div className={style.platforms} dangerouslySetInnerHTML={{ __html: videogame.platforms}}></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <div
+                            className={style.containerDescription}
+                            dangerouslySetInnerHTML={{ __html: videogame.description }}
+                        ></div>
                     </div>
                 )
                 }
